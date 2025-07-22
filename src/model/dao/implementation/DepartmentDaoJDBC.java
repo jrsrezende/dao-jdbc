9f645155617176eb35a2f4a12fa3f55f8a06f45b
João Rezende <joaoricardorezende@gmail.com>
2025-07-22 20:30:17 -0300

    Add DaoFactory class and implementation package

---
+++ src/model/dao/implementation/DepartmentDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.implementation;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findbyId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+}
